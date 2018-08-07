dc097b0ea208cb79ba053fa84be0912281943d45
Rainbwo <1258280950@qq.com>
2018-08-07 10:13:21 +0800

    修改摄像头获取程序。

--- V4L2Capture/main.cpp
+++ V4L2Capture/main.cpp
@@ -3,10 +3,17 @@
 int main()
 {
     cv::Mat frame;
-    V4L2Capture Capture(0);
+//    V4L2Capture Cap(0);
+//    Cap.v4l2control(V4L2_CID_EXPOSURE_AUTO,V4L2_EXPOSURE_MANUAL);  //设置曝光率，先设置为手动曝光，再设置为曝光值
+//    Cap.v4l2control(V4L2_CID_EXPOSURE_ABSOLUTE,50);
+
+
+    cv::VideoCapture Capture(0);
     while(1)
     {
-        frame = Capture.grap();
+//        frame = Cap.grap();
+
+        Capture>>frame;
         cv::imshow("frame",frame);
         if(cv::waitKey(5) == 'q') break;
     }
